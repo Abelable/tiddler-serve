e5212afdede994c996c1e20e29f500674db7bb6b
Administrator <admin@example.com>
2022-12-29 11:39:17 +0800

    feat: 商家入驻

--- app/Http/Controllers/V1/ShopController.php
+++ app/Http/Controllers/V1/ShopController.php
@@ -24,22 +24,22 @@ class ShopController extends Controller
         $merchant->user_id = $this->userId();
         $merchant->type = $input->type;
         if ($input->type === 2) {
-            $merchant->company_name = $input->company_name;
-            $merchant->business_license_photo = $input->business_license_photo;
+            $merchant->company_name = $input->companyName;
+            $merchant->business_license_photo = $input->businessLicensePhoto;
         }
-        $merchant->region_list = $input->region_list;
-        $merchant->address_detail = $input->address_detail;
+        $merchant->region_list = $input->regionList;
+        $merchant->address_detail = $input->addressDetail;
         $merchant->name = $input->name;
         $merchant->mobile = $input->mobile;
         $merchant->email = $input->email;
-        $merchant->id_card_number = $input->id_card_number;
-        $merchant->id_card_front_photo = $input->id_card_front_photo;
-        $merchant->id_card_back_photo = $input->id_card_back_photo;
-        $merchant->hold_id_card_photo = $input->hold_id_card_photo;
-        $merchant->bank_card_number = $input->bank_card_number;
-        $merchant->bank_name = $input->bank_name;
-        $merchant->shop_name = $input->shop_name;
-        $merchant->shop_category_id = $input->shop_category_id;
+        $merchant->id_card_number = $input->idCardNumber;
+        $merchant->id_card_front_photo = $input->idCardFrontPhoto;
+        $merchant->id_card_back_photo = $input->idCardBackPhoto;
+        $merchant->hold_id_card_photo = $input->holdIdCardPhoto;
+        $merchant->bank_card_number = $input->bankCardNumber;
+        $merchant->bank_name = $input->bankName;
+        $merchant->shop_name = $input->shopName;
+        $merchant->shop_category_id = $input->shopCategoryId;
         $merchant->save();
 
         return $this->success();
--- app/Utils/Inputs/MerchantSettleInInput.php
+++ app/Utils/Inputs/MerchantSettleInInput.php
@@ -4,46 +4,44 @@ namespace App\Utils\Inputs;
 
 class MerchantSettleInInput extends BaseInput
 {
-    public $user_id;
-    public $status;
-    public $failure_reason;
+    public $userId;
     public $type;
-    public $company_name;
-    public $region_list;
-    public $address_detail;
-    public $business_license_photo;
+    public $companyName;
+    public $regionList;
+    public $addressDetail;
+    public $businessLicensePhoto;
     public $name;
     public $mobile;
     public $email;
-    public $id_card_number;
-    public $id_card_front_photo;
-    public $id_card_back_photo;
-    public $hold_id_card_photo;
-    public $bank_card_number;
-    public $bank_name;
-    public $shop_name;
-    public $shop_category_id;
+    public $idCardNumber;
+    public $idCardFrontPhoto;
+    public $idCardBackPhoto;
+    public $holdIdCardPhoto;
+    public $bankCardNumber;
+    public $bankName;
+    public $shopName;
+    public $shopCategoryId;
 
     public function rules()
     {
         return [
-            'user_id' => 'required|integer|digits_between:1,20',
+            'userId' => 'required|integer|digits_between:1,20',
             'type' => 'required|integer|in:1,2',
-            'company_name' => 'string',
-            'region_list' => 'required|string',
-            'address_detail' => 'required|string',
-            'business_license_photo' => 'string',
+            'companyName' => 'string',
+            'regionList' => 'required|string',
+            'addressDetail' => 'required|string',
+            'businessLicensePhoto' => 'string',
             'name' => 'required|string',
             'mobile' => 'required|regex:/^1[345789][0-9]{9}$/',
             'email' => 'required|regex:/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/',
-            'id_card_number' => 'required|regex:/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/',
-            'id_card_front_photo' => 'required|string',
-            'id_card_back_photo' => 'required|string',
-            'hold_id_card_photo' => 'required|string',
-            'bank_card_number' => 'required|regex:/^([1-9]{1})(\d{15}|\d{16}|\d{18})$/',
-            'bank_name' => 'required|string',
-            'shop_name' => 'required|string',
-            'shop_category_id' => 'required|integer|digits_between:1,20',
+            'idCardNumber' => 'required|regex:/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/',
+            'idCardFrontPhoto' => 'required|string',
+            'idCardBackPhoto' => 'required|string',
+            'holdIdCardPhoto' => 'required|string',
+            'bankCardNumber' => 'required|regex:/^([1-9]{1})(\d{15}|\d{16}|\d{18})$/',
+            'bankName' => 'required|string',
+            'shopName' => 'required|string',
+            'shopCategoryId' => 'required|integer|digits_between:1,20',
         ];
     }
 }
