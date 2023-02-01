5f22a731f50e1e5a7b715d09f8a52061bca0bd9e
Administrator <admin@example.com>
2023-02-01 16:27:28 +0800

    feat: 商品

---
+++ app/Models/Commodity.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * App\Models\Commodity
+ *
+ * @property int $id
+ * @property int $user_id 用户id
+ * @property int $shop_id 店铺id
+ * @property int $status 申请状态：0-待审核，1-审核通过，2-下架
+ * @property string $failure_reason 审核失败原因
+ * @property string $video 主图视频
+ * @property string $image_list 主图图片
+ * @property string $name 商品名称
+ * @property int $freight_template_id 运费模板id：0-包邮
+ * @property int $category_id 商品分类id
+ * @property int $return_address_id 退货地址id
+ * @property float $price 商品价格
+ * @property float $market_price 市场价格
+ * @property int $stock 商品库存
+ * @property float $commission_rate 推广佣金比例
+ * @property string $detail_image_list 商品详情图片
+ * @property string $spec_list 商品规格列表，使用场景：编辑商品信息
+ * @property string $sku_list 商品sku，使用场景：购买商品
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Commodity onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereDetailImageList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereFailureReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereFreightTemplateId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereImageList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereMarketPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereReturnAddressId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereShopId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereSkuList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereSpecList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Commodity whereVideo($value)
+ * @method static \Illuminate\Database\Query\Builder|Commodity withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Commodity withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Commodity extends BaseModel
+{
+
+}
---
+++ database/migrations/2023_02_01_162052_create_commodities_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommoditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commodities', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment('用户id');
+            $table->integer('shop_id')->comment('店铺id');
+            $table->integer('status')->default(0)->comment('申请状态：0-待审核，1-审核通过，2-下架');
+            $table->string('failure_reason')->default('')->comment('审核失败原因');
+            $table->string('video')->default('')->comment('主图视频');
+            $table->string('image_list')->comment('主图图片');
+            $table->string('name')->comment('商品名称');
+            $table->integer('freight_template_id')->default(0)->comment('运费模板id：0-包邮');
+            $table->integer('category_id')->comment('商品分类id');
+            $table->integer('return_address_id')->comment('退货地址id');
+            $table->float('price')->comment('商品价格');
+            $table->float('market_price')->default(0)->comment('市场价格');
+            $table->integer('stock')->comment('商品库存');
+            $table->float('commission_rate')->default(0)->comment('推广佣金比例');
+            $table->string('detail_image_list')->comment('商品详情图片');
+            $table->longText('spec_list')->comment('商品规格列表，使用场景：编辑商品信息');
+            $table->longText('sku_list')->comment('商品sku，使用场景：购买商品');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commodities');
+    }
+}
