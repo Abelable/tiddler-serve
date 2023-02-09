4f588c8a53506088a2de379adf50ce8bac4c7838
Administrator <admin@example.com>
2023-02-09 16:31:53 +0800

    feat: 购物车

--- app/Http/Controllers/V1/CartController.php
+++ app/Http/Controllers/V1/CartController.php
@@ -8,9 +8,21 @@ use App\Services\CartService;
 use App\Services\GoodsService;
 use App\Utils\CodeResponse;
 use App\Utils\Inputs\CartAddInput;
+use App\Utils\Inputs\CartEditInput;
 
 class CartController extends Controller
 {
+    public function goodsNumber()
+    {
+        $number = CartService::getInstance()->cartGoodsNumber($this->userId());
+        return $this->success($number);
+    }
+
+    public function list()
+    {
+
+    }
+
     public function add()
     {
         /** @var CartAddInput $input */
@@ -18,7 +30,58 @@ class CartController extends Controller
         $selectedSkuIndex = $input->selectedSkuIndex;
         $number = $input->number;
 
-        $goods = GoodsService::getInstance()->getGoodsById($input->goodsId);
+        [$goods, $skuList] = $this->validateCartGoodsStatus($input->goodsId, $selectedSkuIndex, $number);
+
+        $cart = CartService::getInstance()->getExistCart($goods->id, $selectedSkuIndex);
+        if (!is_null($cart)) {
+            $cart->number = $cart->number + $number;
+        } else {
+            $cart = Cart::new();
+            $cart->user_id = $this->userId();
+            $cart->shop_id = $goods->shop_id;
+            $cart->goods_id = $goods->id;
+            $cart->goods_image = $goods->image;
+            $cart->goods_name = $goods->name;
+            if ($selectedSkuIndex != -1 && count($skuList) != 0) {
+                $cart->selected_sku_name = $skuList[$selectedSkuIndex]->name;
+                $cart->selected_sku_index = $selectedSkuIndex;
+                $cart->price = $skuList[$selectedSkuIndex]->price;
+            } else {
+                $cart->price = $goods->price;
+            }
+            $cart->market_price = $goods->market_price;
+            $cart->number = $number;
+        }
+        $cart->save();
+
+        return  $this->goodsNumber();
+    }
+
+    public function edit()
+    {
+        /** @var CartEditInput $input */
+        $input = CartEditInput::new();
+        $selectedSkuIndex = $input->selectedSkuIndex;
+        $number = $input->number;
+
+        $cart = CartService::getInstance()->getExistCart($input->goodsId, $selectedSkuIndex);
+        if (!is_null($cart)) {
+            return $this->fail(CodeResponse::DATA_EXISTED, '购物车中已存在当前规格商品');
+        }
+
+        $this->validateCartGoodsStatus($input->goodsId, $selectedSkuIndex, $number);
+
+        $cart = CartService::getInstance()->getCartById($input->id);
+        $cart->selected_sku_index = $selectedSkuIndex;
+        $cart->number = $number;
+        $cart->save();
+
+        return  $this->goodsNumber();
+    }
+
+    private function validateCartGoodsStatus($goodsId, $selectedSkuIndex, $number)
+    {
+        $goods = GoodsService::getInstance()->getGoodsById($goodsId);
         if (is_null($goods) || $goods->status != 1) {
             return $this->fail(CodeResponse::NOT_FOUND, '当前商品不存在');
         }
@@ -34,30 +97,6 @@ class CartController extends Controller
                 return $this->fail(CodeResponse::CART_INVALID_OPERATION, '所选规格库存不足');
             }
         }
-
-        $cart = Cart::new();
-        $cart->user_id = $this->userId();
-        $cart->shop_id = $goods->shop_id;
-        $cart->goods_id = $goods->id;
-        $cart->goods_image = $goods->image;
-        $cart->goods_name = $goods->name;
-        if ($selectedSkuIndex != -1 && count($skuList) != 0) {
-            $cart->selected_sku_name = $skuList[$selectedSkuIndex]->name;
-            $cart->selected_sku_index = $selectedSkuIndex;
-            $cart->price = $skuList[$selectedSkuIndex]->price;
-        } else {
-            $cart->price = $goods->price;
-        }
-        $cart->market_price = $goods->market_price;
-        $cart->number = $number;
-        $cart->save();
-
-        return  $this->goodsNumber();
-    }
-
-    public function goodsNumber()
-    {
-        $number = CartService::getInstance()->cartGoodsNumber($this->userId());
-        return $this->success($number);
+        return [$goods, $skuList];
     }
 }
--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -10,4 +10,16 @@ class CartService extends BaseService
     {
         return Cart::query()->where('user_id', $userId)->sum('number');
     }
+
+    public function getExistCart($goodsId, $selectedSkuIndex, $columns = ['*'])
+    {
+        return Cart::query()
+                ->where('goods_id', $goodsId)
+                ->where('selected_sku_index', $selectedSkuIndex)
+                ->first($columns);
+    }
+    public function getCartById($id, $columns = ['*'])
+    {
+        return Cart::query()->find($id, $columns);
+    }
 }
