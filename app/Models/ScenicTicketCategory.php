ca498fa73783942744d2783157e9556461a07e31
Administrator <admin@example.com>
2023-04-11 15:33:22 +0800

    feat: 景区门票

---
+++ app/Models/ScenicTicketCategory.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Models;
+
+class ScenicTicketCategory extends BaseModel
+{
+}
--- database/migrations/2023_04_10_115305_create_scenic_tickets_table.php
+++ database/migrations/2023_04_10_115305_create_scenic_tickets_table.php
@@ -17,7 +17,7 @@ class CreateScenicTicketsTable extends Migration
             $table->id();
             $table->integer('user_id')->comment('用户id');
             $table->integer('provider_id')->comment('供应商id');
-            $table->string('scenic_ids')->comment('景点id列表');
+            $table->integer('category_id')->comment('门票分类id');
             $table->integer('type')->default(1)->comment('门票类型：1-单景点门票，2-多景点联票');
             $table->integer('status')->default(0)->comment('申请状态：0-待审核，1-审核通过，2-审核未通过，3-下架');
             $table->string('failure_reason')->default('')->comment('审核失败原因');
