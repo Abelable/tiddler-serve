4a6ec2c1392fa5bb8f5d1ec5b4b9dbf03afa7ce5
Administrator <admin@example.com>
2023-06-06 23:03:03 +0800

    feat: 景区服务商入驻

--- app/Http/Controllers/V1/ScenicProviderController.php
+++ app/Http/Controllers/V1/ScenicProviderController.php
@@ -40,7 +40,6 @@ class ScenicProviderController extends Controller
             'id' => $provider->id,
             'status' => $provider->status,
             'failureReason' => $provider->failure_reason,
-            'type' => $provider->type,
             'orderId' => $providerOrder->id
         ] : null);
     }
