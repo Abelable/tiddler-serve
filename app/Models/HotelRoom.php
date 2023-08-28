1fc49b8e8465b006d55c9ec40aa6b1ca90f4c690
Administrator <admin@example.com>
2023-08-28 14:56:49 +0800

    feat: 酒店房间

---
+++ app/Models/HotelRoom.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * App\Models\HotelRoom
+ *
+ * @property int $id
+ * @property int $user_id 用户id
+ * @property int $provider_id 供应商id
+ * @property int $shop_id 店铺id
+ * @property int $hotel_id 酒店id
+ * @property int $type_id 房间类型id
+ * @property int $status 申请状态：0-待审核，1-审核通过，2-审核未通过，3-下架
+ * @property string $failure_reason 审核失败原因
+ * @property float $sales_commission_rate 销售佣金比例
+ * @property float $promotion_commission_rate 推广佣金比例
+ * @property int $breakfast_num 早餐份数
+ * @property int $guest_num 可入住客人数量
+ * @property int $cancellable 免费取消：0-不可取消，1-可免费取消
+ * @property string $price_list 价格列表：分时间段设置价格
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom newQuery()
+ * @method static \Illuminate\Database\Query\Builder|HotelRoom onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom query()
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereBreakfastNum($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereCancellable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereFailureReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereGuestNum($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereHotelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom wherePriceList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom wherePromotionCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereSalesCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereShopId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereTypeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HotelRoom whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|HotelRoom withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|HotelRoom withoutTrashed()
+ * @mixin \Eloquent
+ */
+class HotelRoom extends BaseModel
+{
+}
---
+++ database/migrations/2023_08_28_142204_create_hotel_rooms_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotel_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment('用户id');
+            $table->integer('provider_id')->comment('供应商id');
+            $table->integer('shop_id')->comment('店铺id');
+            $table->integer('hotel_id')->comment('酒店id');
+            $table->integer('type_id')->comment('房间类型id');
+            $table->integer('status')->default(0)->comment('申请状态：0-待审核，1-审核通过，2-审核未通过，3-下架');
+            $table->string('failure_reason')->default('')->comment('审核失败原因');
+            $table->float('sales_commission_rate')->comment('销售佣金比例');
+            $table->float('promotion_commission_rate')->comment('推广佣金比例');
+            $table->integer('breakfast_num')->comment('早餐份数');
+            $table->integer('guest_num')->comment('可入住客人数量');
+            $table->integer('cancellable')->comment('免费取消：0-不可取消，1-可免费取消');
+            $table->longText('price_list')->comment('价格列表：分时间段设置价格');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel_rooms');
+    }
+}
