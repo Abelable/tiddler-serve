74c297812d2d82eee7a90e1f7dd8d0262b7c0aac
Administrator <admin@example.com>
2023-10-11 11:51:40 +0800

    feat: 代金券订单

--- app/Models/OrderMealTicket.php
+++ app/Models/OrderMealTicket.php
@@ -7,11 +7,21 @@ namespace App\Models;
  *
  * @property int $id
  * @property int $order_id 订单id
- * @property string $restaurant_name 门店名称
- * @property int $ticket_id 代金券id
- * @property float $ticket_price 代金券售价
- * @property float $ticket_original_price 代金券抵扣价格
  * @property int $number 代金券数量
+ * @property int $ticket_id 代金券id
+ * @property float $price 代金券售价
+ * @property float $original_price 代金券抵扣价格
+ * @property int $validity_days 有效天数
+ * @property string $validity_start_time 范围有效期开始时间
+ * @property string $validity_end_time 范围有效期结束时间
+ * @property int $buy_limit 限购数量
+ * @property int $per_table_usage_limit 单桌使用数量限制
+ * @property int $overlay_usage_limit 叠加使用数量限制
+ * @property string $use_time_list 使用时间范围
+ * @property string $inapplicable_products 不可用商品列表
+ * @property int $box_available 包厢是否可用：0-不可用，1-可用
+ * @property int $need_pre_book 是否需要预定：0-不需要预定，1-需要预定
+ * @property string $use_rules 使用规则
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -19,16 +29,26 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket newQuery()
  * @method static \Illuminate\Database\Query\Builder|OrderMealTicket onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereBoxAvailable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereBuyLimit($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereInapplicableProducts($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereNeedPreBook($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereNumber($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereOrderId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereRestaurantName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereOriginalPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereOverlayUsageLimit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket wherePerTableUsageLimit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket wherePrice($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereTicketId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereTicketOriginalPrice($value)
- * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereTicketPrice($value)
  * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereUseRules($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereUseTimeList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereValidityDays($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereValidityEndTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderMealTicket whereValidityStartTime($value)
  * @method static \Illuminate\Database\Query\Builder|OrderMealTicket withTrashed()
  * @method static \Illuminate\Database\Query\Builder|OrderMealTicket withoutTrashed()
  * @mixin \Eloquent
--- database/migrations/2023_10_11_105236_create_order_meal_tickets_table.php
+++ database/migrations/2023_10_11_105236_create_order_meal_tickets_table.php
@@ -16,11 +16,21 @@ class CreateOrderMealTicketsTable extends Migration
         Schema::create('order_meal_tickets', function (Blueprint $table) {
             $table->id();
             $table->integer('order_id')->comment('订单id');
-            $table->string('restaurant_name')->comment('门店名称');
-            $table->integer('ticket_id')->comment('代金券id');
-            $table->float('ticket_price')->comment('代金券售价');
-            $table->float('ticket_original_price')->comment('代金券抵扣价格');
             $table->integer('number')->comment('代金券数量');
+            $table->integer('ticket_id')->comment('代金券id');
+            $table->float('price')->comment('代金券售价');
+            $table->float('original_price')->comment('代金券抵扣价格');
+            $table->integer('validity_days')->default(0)->comment('有效天数');
+            $table->string('validity_start_time')->default('')->comment('范围有效期开始时间');
+            $table->string('validity_end_time')->default('')->comment('范围有效期结束时间');
+            $table->integer('buy_limit')->default(0)->comment('限购数量');
+            $table->integer('per_table_usage_limit')->default(0)->comment('单桌使用数量限制');
+            $table->integer('overlay_usage_limit')->default(0)->comment('叠加使用数量限制');
+            $table->longText('use_time_list')->comment('使用时间范围');
+            $table->longText('inapplicable_products')->comment('不可用商品列表');
+            $table->integer('box_available')->default(0)->comment('包厢是否可用：0-不可用，1-可用');
+            $table->integer('need_pre_book')->default(0)->comment('是否需要预定：0-不需要预定，1-需要预定');
+            $table->longText('use_rules')->comment('使用规则');
             $table->timestamps();
             $table->softDeletes();
         });
