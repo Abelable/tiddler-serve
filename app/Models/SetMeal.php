d491d21a4b6d79f5e1be6abe3c94b475ede24c50
Administrator <admin@example.com>
2023-10-12 10:55:15 +0800

    feat: 餐饮套餐

---
+++ app/Models/SetMeal.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * App\Models\SetMeal
+ *
+ * @property int $id
+ * @property int $user_id 用户id
+ * @property int $provider_id 供应商id
+ * @property int $status 申请状态：0-待审核，1-审核通过，2-审核未通过，3-下架
+ * @property string $failure_reason 审核失败原因
+ * @property string $cover 套餐图片
+ * @property string $name 套餐名称
+ * @property float $price 套餐价格
+ * @property float $original_price 套餐原价
+ * @property float $sales_commission_rate 销售佣金比例
+ * @property float $promotion_commission_rate 推广佣金比例
+ * @property int $sales_volume 销量
+ * @property string $package_details 套餐详情
+ * @property int $validity_days 有效天数
+ * @property string $validity_start_time 范围有效期开始时间
+ * @property string $validity_end_time 范围有效期结束时间
+ * @property int $buy_limit 限购数量
+ * @property int $per_table_usage_limit 单桌使用数量限制
+ * @property string $use_time_list 使用时间范围
+ * @property int $need_pre_book 是否需要预定：0-不需要预定，1-需要预定
+ * @property string $use_rules 使用规则
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal newQuery()
+ * @method static \Illuminate\Database\Query\Builder|SetMeal onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal query()
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereBuyLimit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereCover($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereFailureReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereNeedPreBook($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereOriginalPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal wherePackageDetails($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal wherePerTableUsageLimit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal wherePromotionCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereSalesCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereSalesVolume($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereUseRules($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereUseTimeList($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereValidityDays($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereValidityEndTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SetMeal whereValidityStartTime($value)
+ * @method static \Illuminate\Database\Query\Builder|SetMeal withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|SetMeal withoutTrashed()
+ * @mixin \Eloquent
+ */
+class SetMeal extends BaseModel
+{
+}
---
+++ database/migrations/2023_10_12_101136_create_set_meals_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSetMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('set_meals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment('用户id');
+            $table->integer('provider_id')->comment('供应商id');
+            $table->integer('status')->default(0)->comment('申请状态：0-待审核，1-审核通过，2-审核未通过，3-下架');
+            $table->string('failure_reason')->default('')->comment('审核失败原因');
+            $table->string('cover')->comment('套餐图片');
+            $table->string('name')->comment('套餐名称');
+            $table->float('price')->comment('套餐价格');
+            $table->float('original_price')->comment('套餐原价');
+            $table->float('sales_commission_rate')->comment('销售佣金比例');
+            $table->float('promotion_commission_rate')->comment('推广佣金比例');
+            $table->integer('sales_volume')->default(0)->comment('销量');
+            $table->longText('package_details')->comment('套餐详情');
+            $table->integer('validity_days')->default(0)->comment('有效天数');
+            $table->string('validity_start_time')->default('')->comment('范围有效期开始时间');
+            $table->string('validity_end_time')->default('')->comment('范围有效期结束时间');
+            $table->integer('buy_limit')->default(0)->comment('限购数量');
+            $table->integer('per_table_usage_limit')->default(0)->comment('单桌使用数量限制');
+            $table->longText('use_time_list')->comment('使用时间范围');
+            $table->integer('need_pre_book')->default(0)->comment('是否需要预定：0-不需要预定，1-需要预定');
+            $table->longText('use_rules')->comment('使用规则');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('set_meals');
+    }
+}
