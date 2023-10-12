0b757b0491f76ff0767bda180b41cfa7dc91539c
Administrator <admin@example.com>
2023-10-12 11:20:31 +0800

    feat: 餐饮套餐

--- app/Http/Controllers/V1/SetMealController.php
+++ app/Http/Controllers/V1/SetMealController.php
@@ -76,14 +76,14 @@ class SetMealController extends Controller
     {
         $id = $this->verifyRequiredId('id');
 
-        $setMeal = SetMealService::getInstance()->getTicketById($id);
+        $setMeal = SetMealService::getInstance()->getSetMealById($id);
         if (is_null($setMeal)) {
-            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮代金券不存在');
+            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮套餐不存在');
         }
 
         $setMeal['restaurantIds'] = $setMeal->restaurantIds();
+        $setMeal->package_details = json_decode($setMeal->package_details);
         $setMeal->use_time_list = json_decode($setMeal->use_time_list) ?: [];
-        $setMeal->inapplicable_products = json_decode($setMeal->inapplicable_products) ?: [];
         $setMeal->use_rules = json_decode($setMeal->use_rules) ?: [];
 
         return $this->success($setMeal);
@@ -96,12 +96,12 @@ class SetMealController extends Controller
 
         $providerId = $this->user()->cateringProvider->id;
         if ($providerId == 0) {
-            return $this->fail(CodeResponse::FORBIDDEN, '您不是服务商，无法上传餐饮代金券');
+            return $this->fail(CodeResponse::FORBIDDEN, '您不是服务商，无法上传餐饮套餐');
         }
 
         DB::transaction(function () use ($providerId, $input) {
-            $setMeal = SetMealService::getInstance()->createTicket($this->userId(), $providerId, $input);
-            RestaurantSetMealService::getInstance()->createRestaurantTickets($setMeal->id, $input->restaurantIds);
+            $setMeal = SetMealService::getInstance()->createSetMeal($this->userId(), $providerId, $input);
+            RestaurantSetMealService::getInstance()->createRestaurantSetMeals($setMeal->id, $input->restaurantIds);
         });
 
         return $this->success();
@@ -113,14 +113,14 @@ class SetMealController extends Controller
         /** @var SetMealInput $input */
         $input = SetMealInput::new();
 
-        $setMeal = SetMealService::getInstance()->getUserTicket($this->userId(), $id);
+        $setMeal = SetMealService::getInstance()->getUserSetMeal($this->userId(), $id);
         if (is_null($setMeal)) {
-            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮代金券不存在');
+            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮套餐不存在');
         }
 
         DB::transaction(function () use ($input, $setMeal) {
-            SetMealService::getInstance()->updateTicket($setMeal, $input);
-            RestaurantSetMealService::getInstance()->updateRestaurantTickets($setMeal->id, $input->restaurantIds);
+            SetMealService::getInstance()->updateSetMeal($setMeal, $input);
+            RestaurantSetMealService::getInstance()->updateRestaurantSetMeals($setMeal->id, $input->restaurantIds);
         });
 
         return $this->success();
@@ -130,12 +130,12 @@ class SetMealController extends Controller
     {
         $id = $this->verifyRequiredId('id');
 
-        $setMeal = SetMealService::getInstance()->getUserTicket($this->userId(), $id);
+        $setMeal = SetMealService::getInstance()->getUserSetMeal($this->userId(), $id);
         if (is_null($setMeal)) {
-            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮代金券不存在');
+            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮套餐不存在');
         }
         if ($setMeal->status != 3) {
-            return $this->fail(CodeResponse::FORBIDDEN, '非下架餐饮代金券，无法上架');
+            return $this->fail(CodeResponse::FORBIDDEN, '非下架餐饮套餐，无法上架');
         }
         $setMeal->status = 1;
         $setMeal->save();
@@ -147,12 +147,12 @@ class SetMealController extends Controller
     {
         $id = $this->verifyRequiredId('id');
 
-        $setMeal = SetMealService::getInstance()->getUserTicket($this->userId(), $id);
+        $setMeal = SetMealService::getInstance()->getUserSetMeal($this->userId(), $id);
         if (is_null($setMeal)) {
-            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮代金券不存在');
+            return $this->fail(CodeResponse::NOT_FOUND, '当前餐饮套餐不存在');
         }
         if ($setMeal->status != 1) {
-            return $this->fail(CodeResponse::FORBIDDEN, '非售卖中餐饮代金券，无法下架');
+            return $this->fail(CodeResponse::FORBIDDEN, '非售卖中餐饮套餐，无法下架');
         }
         $setMeal->status = 3;
         $setMeal->save();
@@ -165,8 +165,8 @@ class SetMealController extends Controller
         $id = $this->verifyRequiredId('id');
 
         DB::transaction(function () use ($id) {
-            SetMealService::getInstance()->deleteTicket($this->userId(), $id);
-            RestaurantSetMealService::getInstance()->deleteByTicketId($id);
+            SetMealService::getInstance()->deleteSetMeal($this->userId(), $id);
+            RestaurantSetMealService::getInstance()->deleteBySetMealId($id);
         });
 
         return $this->success();
