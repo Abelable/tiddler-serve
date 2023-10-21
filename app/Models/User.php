9f6040acc5f82889b31fbcafa2c7af29b42a79c9
Administrator <admin@example.com>
2023-10-21 14:00:57 +0800

    feat: 用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,11 +20,15 @@ use Illuminate\Notifications\Notifiable;
  * @property string $openid 小程序openid
  * @property string $unionid 微信unionid
  * @property int $gender 性别：0-未知，1-男，2-女
- * @property string $signature 签名
  * @property string $bg 背景图
+ * @property string $birthday 生日
+ * @property string $constellation 星座
+ * @property string $career 职业
+ * @property string $signature 签名
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \App\Models\AuthInfo|null $authInfo
  * @property-read \App\Models\CateringProvider|null $cateringProvider
  * @property-read \App\Models\HotelProvider|null $hotelProvider
  * @property-read \App\Models\Merchant|null $merchant
@@ -39,6 +43,9 @@ use Illuminate\Notifications\Notifiable;
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
  * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereBg($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCareer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereConstellation($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereGender($value)
@@ -114,5 +121,10 @@ class User extends BaseModel implements JWTSubject, AuthenticatableContract, Aut
     public function cateringProvider()
     {
         return $this->hasOne(CateringProvider::class, 'user_id')->where('status', 2);
+    }
+
+    public function authInfo()
+    {
+        return $this->hasOne(AuthInfo::class, 'user_id')->where('status', 1);
     }
 }
--- database/migrations/2022_09_05_061958_create_users_table.php
+++ database/migrations/2022_09_05_061958_create_users_table.php
@@ -22,8 +22,11 @@ class CreateUsersTable extends Migration
             $table->string('openid')->unique()->comment('小程序openid');
             $table->string('unionid')->unique()->comment('微信unionid');
             $table->integer('gender')->default(0)->comment('性别：0-未知，1-男，2-女');
-            $table->string('signature')->default('')->comment('签名');
             $table->string('bg')->default('')->comment('背景图');
+            $table->string('birthday')->default('')->comment('生日');
+            $table->string('constellation')->default('')->comment('星座');
+            $table->string('career')->default('')->comment('职业');
+            $table->string('signature')->default('')->comment('签名');
             $table->timestamps();
             $table->softDeletes();
         });
