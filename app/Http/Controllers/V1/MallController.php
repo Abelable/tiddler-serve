210719abfaf8902010642ad5e61ff5da3d641178
Administrator <admin@example.com>
2023-11-13 14:29:36 +0800

    feat: 商城商品列表

--- app/Http/Controllers/V1/MallController.php
+++ app/Http/Controllers/V1/MallController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Services\MallService;
+use App\Services\ShopService;
 use App\Utils\Inputs\PageInput;
 use Illuminate\Support\Facades\DB;
 
@@ -18,12 +19,14 @@ class MallController extends Controller
 
         $scenicColumns = [
             'id',
+            DB::raw('NULL as shop_id'),
             'status',
             'name',
             DB::raw('NULL as cover'),
             DB::raw('NULL as image'),
             'image_list',
             DB::raw('NULL as price'),
+            DB::raw('NULL as market_price'),
             'longitude',
             'latitude',
             'address',
@@ -31,12 +34,14 @@ class MallController extends Controller
         ];
         $hotelColumns = [
             'id',
+            DB::raw('NULL as shop_id'),
             'status',
             'name',
             'cover',
             DB::raw('NULL as image'),
             DB::raw('NULL as image_list'),
             'price',
+            DB::raw('NULL as market_price'),
             'longitude',
             'latitude',
             'address',
@@ -44,11 +49,14 @@ class MallController extends Controller
         ];
         $restaurantColumns = [
             'id',
+            DB::raw('NULL as shop_id'),
             DB::raw('NULL as status'),
             'name',
             'cover',
             DB::raw('NULL as image'),
             DB::raw('NULL as image_list'),
+            'price',
+            DB::raw('NULL as market_price'),
             'longitude',
             'latitude',
             'address',
@@ -56,11 +64,14 @@ class MallController extends Controller
         ];
         $goodsColumns = [
             'id',
+            'shop_id',
             'status',
             'name',
             DB::raw('NULL as cover'),
             'image',
             'image_list',
+            'price',
+            'market_price',
             DB::raw('NULL as longitude'),
             DB::raw('NULL as latitude'),
             DB::raw('NULL as address'),
@@ -68,6 +79,17 @@ class MallController extends Controller
         ];
 
         $page = MallService::getInstance()->pageList($input, $scenicColumns, $hotelColumns, $restaurantColumns, $goodsColumns);
-        return $this->successPaginate($page);
+        $list = collect($page->items())->map(function ($commodity) {
+            if ($commodity['type'] == 1) {
+                $commodity['image_list'] = json_decode($commodity['image_list']);
+            }
+            if ($commodity['type'] == 4 && $commodity->shop_id != 0) {
+                $shopInfo = ShopService::getInstance()->getShopById($commodity->shop_id, ['id', 'type', 'avatar', 'name']);
+                $commodity['shop_info'] = $shopInfo;
+            }
+            return $commodity;
+        });
+
+        return $this->success($this->paginate($page, $list));
     }
 }
