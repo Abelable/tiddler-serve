4c2efef150560b81b55a09dd2934303273f15b21
Administrator <admin@example.com>
2024-01-03 10:41:57 +0800

    feat: 商品分类

--- app/Models/GoodsCategory.php
+++ app/Models/GoodsCategory.php
@@ -6,7 +6,12 @@ namespace App\Models;
  * App\Models\GoodsCategory
  *
  * @property int $id
+ * @property int $shop_category_id 店铺分类id
  * @property string $name 商品分类名称
+ * @property int $min_sales_commission_rate 最小销售佣金比例
+ * @property int $max_sales_commission_rate 最大销售佣金比例
+ * @property int $min_promotion_commission_rate 最小推广佣金比例
+ * @property int $max_promotion_commission_rate 最大推广佣金比例
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -17,7 +22,12 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereMaxPromotionCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereMaxSalesCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereMinPromotionCommissionRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereMinSalesCommissionRate($value)
  * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereShopCategoryId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|GoodsCategory whereUpdatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|GoodsCategory withTrashed()
  * @method static \Illuminate\Database\Query\Builder|GoodsCategory withoutTrashed()
