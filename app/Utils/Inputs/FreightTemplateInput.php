abb794f5a6bd0cfa3d4e2c35ef5ee1ba0a273652
Administrator <admin@example.com>
2024-02-18 14:13:30 +0800

    feat: 运费模板

--- app/Utils/Inputs/FreightTemplateInput.php
+++ app/Utils/Inputs/FreightTemplateInput.php
@@ -4,26 +4,20 @@ namespace App\Utils\Inputs;
 
 class FreightTemplateInput extends BaseInput
 {
-    public $mode;
     public $name;
     public $title;
     public $computeMode;
     public $freeQuota;
     public $areaList;
-    public $expressList;
-    public $expressTemplateLists;
 
     public function rules()
     {
         return [
-            'mode' => 'required|integer|in:1,2',
             'name' => 'required|string',
             'title' => 'required|string',
             'computeMode' => 'required|integer|in:1,2',
             'freeQuota' => 'numeric',
-            'areaList' => 'required_if:mode,1',
-            'expressList' => 'required_if:mode,1',
-            'expressTemplateLists' => 'required_if:mode,1',
+            'areaList' => 'required|array|min:1',
         ];
     }
 }
--- database/migrations/2023_01_30_144213_create_freight_templates_table.php
+++ database/migrations/2023_01_30_144213_create_freight_templates_table.php
@@ -16,14 +16,11 @@ class CreateFreightTemplatesTable extends Migration
         Schema::create('freight_templates', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->default(0)->comment('用户id');
-            $table->integer('mode')->default(1)->comment('模板类型：1-自定义模板，2-快递模板');
             $table->string('name')->comment('模板名称');
             $table->string('title')->comment('模板标题，可展示在商品详情页');
             $table->integer('compute_mode')->default(1)->comment('计算方式：1-不计重量和件数，2-按商品件数');
             $table->float('free_quota')->default(0)->comment('免费额度');
-            $table->longText('area_list')->comment('自定义模板的配送地区列表');
-            $table->longText('express_list')->comment('自定义模板的快递方式列表');
-            $table->longText('express_template_lists')->comment('快递模板列表');
+            $table->longText('area_list')->comment('配送地区列表');
             $table->timestamps();
             $table->softDeletes();
         });
