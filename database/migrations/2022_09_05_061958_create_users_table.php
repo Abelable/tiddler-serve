4f344b8c9547f29afbf3847be85c569e6864a604
Administrator <admin@example.com>
2024-11-12 09:16:21 +0800

    feat: 用户表

--- database/migrations/2022_09_05_061958_create_users_table.php
+++ database/migrations/2022_09_05_061958_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('nickname')->comment('用户昵称或网络名称');
             $table->string('avatar')->comment('用户头像图片');
-            $table->string('mobile')->unique()->comment('用户手机号码');
-            $table->string('openid')->unique()->comment('小程序openid');
+            $table->string('mobile')->default('')->comment('用户手机号码');
+            $table->string('openid')->default('')->comment('小程序openid');
             $table->integer('gender')->default(0)->comment('性别：0-未知，1-男，2-女');
             $table->string('bg')->default('')->comment('背景图');
             $table->string('birthday')->default('')->comment('生日');
