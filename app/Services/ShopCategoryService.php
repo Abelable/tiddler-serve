cfd07221cb8d18defff88532bf78a22b427d4451
Administrator <admin@example.com>
2024-12-03 12:14:26 +0800

    feat: 店铺分类增加排序跟隐藏

--- app/Services/ShopCategoryService.php
+++ app/Services/ShopCategoryService.php
@@ -24,6 +24,6 @@ class ShopCategoryService extends BaseService
 
     public function getCategoryOptions($columns = ['*'])
     {
-        return ShopCategory::query()->orderBy('id', 'asc')->get($columns);
+        return ShopCategory::query()->orderBy('id', 'asc')->orderBy('sort', 'desc')->get($columns);
     }
 }
