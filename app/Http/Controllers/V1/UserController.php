e0aa8934d243f285089154723d4cb694bfd80452
funguy <1209683835@qq.com>
2025-04-07 09:47:19 +0800

    feat: 用户上级id

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
         $user['promoterId'] = $user->promoterInfo->id ?? 0;
         $user['level'] = $user->promoterInfo->level ?? 0;
-        $user['superiorId'] = $user->superiorId();
+        $user['superiorId'] = $user->superiorId() ?? 0;
 
         $user['authInfoId'] = $user->authInfo->id ?? 0;
 
