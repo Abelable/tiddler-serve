9270fe9e25f3bec198b60c64d1562b0549209f84
funguy <1209683835@qq.com>
2025-06-09 16:46:29 +0800

    feat: banner

--- app/Utils/Inputs/BannerPageInput.php
+++ app/Utils/Inputs/BannerPageInput.php
@@ -4,14 +4,16 @@ namespace App\Utils\Inputs;
 
 class BannerPageInput extends PageInput
 {
+    public $position;
     public $status;
     public $scene;
 
     public function rules()
     {
         return array_merge(parent::rules(), [
+            'position' => 'integer',
             'status' => 'integer|in:1,2',
-            'scene' => 'integer|in:1,2,3,4,5',
+            'scene' => 'integer',
         ]);
     }
 }
