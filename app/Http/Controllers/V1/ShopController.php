d147734b5a667c16d4e6971ae23df4b7f265af6b
funguy <1209683835@qq.com>
2025-11-21 16:41:08 +0800

    feat: 店铺中心

--- app/Http/Controllers/V1/ShopController.php
+++ app/Http/Controllers/V1/ShopController.php
@@ -28,9 +28,8 @@ class ShopController extends Controller
     public function shopInfo()
     {
         $id = $this->verifyRequiredId('id');
-        $columns = ['id', 'category_ids', 'name', 'type', 'logo', 'bg'];
 
-        $shop = ShopService::getInstance()->getShopById($id, $columns);
+        $shop = ShopService::getInstance()->getShopById($id);
         if (is_null($shop)) {
             return $this->fail(CodeResponse::NOT_FOUND, '当前店铺不存在');
         }
@@ -44,10 +43,11 @@ class ShopController extends Controller
     {
         /** @var ShopInput $input */
         $input = ShopInput::new();
+        $id = $this->verifyRequiredId('id');
 
-        $shop = ShopService::getInstance()->getShopByUserId($this->userId());
+        $shop = ShopService::getInstance()->getShopById($id);
         if (is_null($shop)) {
-            return $this->fail(CodeResponse::NOT_FOUND, '您非商家，暂无店铺');
+            return $this->fail(CodeResponse::NOT_FOUND, '当前店铺不存在');
         }
 
         ShopService::getInstance()->updateShopInfo($shop, $input);
