49fb1bd23a4e2cf0416ae51d0ad6b026a86922f3
funguy <1209683835@qq.com>
2025-12-07 11:11:33 +0800

    feat: 表字段优化（售后）

--- app/Models/Refund.php
+++ app/Models/Refund.php
@@ -7,28 +7,30 @@ namespace App\Models;
  *
  * @property int $id
  * @property int $status 申请状态：0-待审核，1-审核通过，等待买家寄回，2-买家已寄出，待确认，3-退款成功，4-审核失败
- * @property string $failure_reason 审核失败原因
+ * @property string|null $failure_reason 审核失败原因
  * @property int $user_id 用户id
  * @property int $shop_id 店铺id
  * @property int $order_id 订单id
  * @property string $order_sn 订单编号
- * @property int $coupon_id 优惠券id
  * @property int $order_goods_id 订单商品id
  * @property int $goods_id 商品id
- * @property float $refund_amount 退款金额
+ * @property int|null $coupon_id 优惠券id
+ * @property string $refund_amount 退款金额
  * @property int $refund_type 售后类型：1-仅退款，2-退货退款
- * @property string $refund_reason 退款说明
- * @property int $refund_address_id 退货地址id
- * @property string $image_list 图片说明
- * @property string $ship_channel 快递公司
- * @property string $ship_code 快递公司编号
- * @property string $ship_sn 快递编号
+ * @property string|null $refund_reason 退款说明
+ * @property mixed|null $image_list 图片说明
+ * @property int|null $refund_address_id 退货地址id
+ * @property string|null $ship_channel 退货快递公司
+ * @property string|null $ship_code 快递公司编码
+ * @property string|null $ship_sn 退货快递单号
+ * @property int|null $reviewer_id 审核管理员ID
+ * @property string|null $reviewed_at 审核时间
+ * @property string|null $refunded_at 退款成功时间
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @method static \Illuminate\Database\Eloquent\Builder|Refund newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Refund newQuery()
- * @method static \Illuminate\Database\Query\Builder|Refund onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Refund query()
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereCouponId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereCreatedAt($value)
@@ -44,6 +46,9 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereRefundAmount($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereRefundReason($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereRefundType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Refund whereRefundedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Refund whereReviewedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Refund whereReviewerId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereShipChannel($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereShipCode($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereShipSn($value)
@@ -51,8 +56,6 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereStatus($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Refund whereUserId($value)
- * @method static \Illuminate\Database\Query\Builder|Refund withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Refund withoutTrashed()
  * @mixin \Eloquent
  */
 class Refund extends BaseModel
--- database/migrations/2024_12_05_131736_create_refunds_table.php
+++ database/migrations/2024_12_05_131736_create_refunds_table.php
@@ -15,23 +15,36 @@ class CreateRefundsTable extends Migration
     {
         Schema::create('refunds', function (Blueprint $table) {
             $table->id();
-            $table->integer('status')->default(0)->comment('申请状态：0-待审核，1-审核通过，等待买家寄回，2-买家已寄出，待确认，3-退款成功，4-审核失败');
-            $table->string('failure_reason')->default('')->comment('审核失败原因');
-            $table->integer('user_id')->comment('用户id');
-            $table->integer('shop_id')->default(0)->comment('店铺id');
-            $table->integer('order_id')->comment('订单id');
-            $table->string('order_sn')->comment('订单编号');
-            $table->integer('coupon_id')->default(0)->comment('优惠券id');
-            $table->integer('order_goods_id')->comment('订单商品id');
-            $table->integer('goods_id')->comment('商品id');
-            $table->float('refund_amount')->comment('退款金额');
-            $table->integer('refund_type')->comment('售后类型：1-仅退款，2-退货退款');
-            $table->string('refund_reason')->comment('退款说明');
-            $table->integer('refund_address_id')->default(0)->comment('退货地址id');
-            $table->string('image_list')->default('')->comment('图片说明');
-            $table->string('ship_channel')->default('')->comment('快递公司');
-            $table->string('ship_code')->default('')->comment('快递公司编号');
-            $table->string('ship_sn')->default('')->comment('快递编号');
+
+            $table->tinyInteger('status')
+                ->default(0)
+                ->comment('申请状态：0-待审核，1-审核通过，等待买家寄回，2-买家已寄出，待确认，3-退款成功，4-审核失败');
+            $table->string('failure_reason', 255)->nullable()->comment('审核失败原因');
+
+            $table->unsignedBigInteger('user_id')->index()->comment('用户id');
+            $table->unsignedBigInteger('shop_id')->default(0)->index()->comment('店铺id');
+            $table->unsignedBigInteger('order_id')->index()->comment('订单id');
+            $table->string('order_sn', 50)->index()->comment('订单编号');
+            $table->unsignedBigInteger('order_goods_id')->index()->comment('订单商品id');
+            $table->unsignedBigInteger('goods_id')->index()->comment('商品id');
+            $table->unsignedBigInteger('coupon_id')->nullable()->comment('优惠券id');
+
+            $table->unsignedDecimal('refund_amount', 10, 2)->default(0)->comment('退款金额');
+
+            $table->tinyInteger('refund_type')->comment('售后类型：1-仅退款，2-退货退款');
+            $table->string('refund_reason', 255)->nullable()->comment('退款说明');
+            $table->json('image_list')->nullable()->comment('图片说明');
+
+            $table->unsignedBigInteger('refund_address_id')->nullable()->comment('退货地址id');
+
+            $table->string('ship_channel', 50)->nullable()->comment('退货快递公司');
+            $table->string('ship_code', 20)->nullable()->comment('快递公司编码');
+            $table->string('ship_sn', 50)->nullable()->comment('退货快递单号');
+
+            $table->unsignedBigInteger('reviewer_id')->nullable()->comment('审核管理员ID');
+            $table->dateTime('reviewed_at')->nullable()->comment('审核时间');
+            $table->dateTime('refunded_at')->nullable()->comment('退款成功时间');
+
             $table->timestamps();
             $table->softDeletes();
         });
