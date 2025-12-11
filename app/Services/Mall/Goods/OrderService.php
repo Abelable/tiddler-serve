0b4b79c2d9777fe3bf3ba4728227c3b963a2a80c
funguy <1209683835@qq.com>
2025-12-11 15:09:56 +0800

    feat: 订单状态排序

--- app/Services/Mall/Goods/OrderService.php
+++ app/Services/Mall/Goods/OrderService.php
@@ -108,7 +108,7 @@ class OrderService extends BaseService
             $query = $query->where('mobile', $input->mobile);
         }
         return $query
-            ->orderByRaw("FIELD(status, 302, 201, 202) DESC")
+            ->orderByRaw("FIELD(status, 202, 201, 302) DESC")
             ->orderBy($input->sort, $input->order)
             ->paginate($input->limit, $columns, 'page', $input->page);
     }
